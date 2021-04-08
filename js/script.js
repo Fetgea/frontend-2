e2209f33921ff43e14df3197f350478bb6af0a98
Dinar <dinar@reaspekt.com>
2021-04-08 17:02:46 +0300

    Второй раунд исправления замечаний

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 let navigation = document.getElementById("navigation");
 let buttonBurger = document.getElementById("buttonBurger");
+let buttonLogin = document.getElementById("buttonLogin");
+
 buttonBurger.addEventListener("click", (evt) => {
     evt.preventDefault();
-    navigation.classList.toggle("removed");
-    buttonBurger.classList.toggle("fixed");
+    let buttonStyle = getComputedStyle(buttonBurger);
+    let fullWidth = buttonBurger.offsetWidth + parseInt(buttonStyle.marginLeft) + parseInt(buttonStyle.marginRight);
+
+    if (navigation.classList.contains("removed")) {
+        navigation.classList.remove("removed");
+        buttonLogin.style.marginRight = fullWidth + "px";
+        buttonBurger.classList.add("fixed");
+    } else {
+        navigation.classList.add("removed");
+        buttonLogin.style.marginRight = 0;
+        buttonBurger.classList.remove("fixed");
+    }    
 })
