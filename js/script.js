8e4fef317c7a66db1c9623cad1bd7925d0ddbb1f
Dinar <dinar@reaspekt.com>
2021-04-14 15:45:33 +0300

    Поправил замечания, поменял профиль autoprefixer

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-let navigation = document.getElementById("navigation");
-let buttonBurger = document.getElementById("buttonBurger");
-let buttonLogin = document.getElementById("buttonLogin");
+let navigation = document.getElementById('navigation');
+let buttonBurger = document.getElementById('buttonBurger');
+let buttonLogin = document.getElementById('buttonLogin');
 
-buttonBurger.addEventListener("click", (evt) => {
+buttonBurger.addEventListener('click', (evt) => {
     evt.preventDefault();
     let buttonStyle = getComputedStyle(buttonBurger);
     let fullWidth = buttonBurger.offsetWidth + parseInt(buttonStyle.marginLeft) + parseInt(buttonStyle.marginRight);
 
-    if (navigation.classList.contains("removed")) {
-        navigation.classList.remove("removed");
-        buttonLogin.style.marginRight = fullWidth + "px";
-        buttonBurger.classList.add("fixed");
+    if (navigation.classList.contains('removed')) {
+        navigation.classList.remove('removed');
+        buttonLogin.style.marginRight = fullWidth + 'px';
+        buttonBurger.classList.add('fixed');
     } else {
-        navigation.classList.add("removed");
+        navigation.classList.add('removed');
         buttonLogin.style.marginRight = 0;
-        buttonBurger.classList.remove("fixed");
+        buttonBurger.classList.remove('fixed');
     }    
 })
