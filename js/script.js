85258c253adeb943057d8d23c0fcde244681e7ba
Dinar <dinar@reaspekt.com>
2021-04-23 13:01:34 +0300

    made testimony elements overlap each other on expansion

--- js/script.js
+++ js/script.js
@@ -17,13 +17,53 @@ buttonBurger.addEventListener('click', (evt) => {
         buttonBurger.classList.remove('fixed');
     }
 });
-$( document ).ready(function() {
-    $(".readMore").readmore({
-        collapsedHeight: 200,
-        speed: 75,
-        lessLink: "<a class='buttonReadMore' href='#'>Hide</a>",
-        moreLink: "<a class='buttonReadMore' href='#'>Read more</a>"  
-      });
+jQuery.fn.extend({
+    getMaxZ : function(){
+        return Math.max.apply(null, jQuery(this).map(function(){
+            var z;
+            return isNaN(z = parseInt(jQuery(this).css("z-index"), 10)) ? 0 : z;
+        }));
+    }
 });
 
-  
+$( document ).ready(function() {
+    
+    $(".readMore").readmore({
+        afterToggle: (trigger, element, expanded) => {
+            let parent = $(element).parent();
+            if ($(trigger).hasClass("hideMore")) {
+                $(element).parent().removeAttr("style");
+                let divWrapper = $(parent).siblings(".testimonyCardAppend");
+                $(divWrapper).remove();
+            }
+        },
+        
+        beforeToggle: (trigger, element, expanded) => {
+            let wrapper = $(element).parents(".testimonyCardWrapper");
+            siblings = $(wrapper).siblings();
+            /*console.log(siblings);*/
+            $(siblings).each((index, element) => {
+                let readmore = $(element).find("div.readMore");
+                if ($(readmore).attr("aria-expanded") == "true") {
+                   let button = $(element).find("a.buttonReadMore");
+                   $(button).trigger("click");
+                }
+            })
+
+            let parent = $(element).parent();
+            let height = parent.outerHeight(true);
+            let width = parent.outerWidth();
+            if ($(trigger).hasClass("readMore")) {
+                let zIndex = $(".testimonyCard").getMaxZ();
+                $(element).parent().css({position: "absolute", width: width, "z-index": zIndex+1});
+                let divWrapper = $(element).parents(".testimonyCardWrapper");
+                $(divWrapper).append(`<div class="testimonyCardAppend" style="height:${height}px"></div>`);
+            }
+            
+        },
+        collapsedHeight: 200,
+        speed: 75,
+        lessLink: "<a class='buttonReadMore hideMore' href='#'>Hide</a>",
+        moreLink: "<a class='buttonReadMore readMore' href='#'>Read more</a>"  
+      });
+});
